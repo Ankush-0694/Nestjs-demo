837bd580ad9961372464d6a22ee241a4cce1251e
Ankush-0694 <ankushkashyap0000@gmail.com>
2022-04-19 15:36:51 +0530

    intial commit with demo modules

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService : AuthService){}
+
+  @Post('signup')
+  signup(@Body() authBody: AuthDto){
+    console.log({ authBody })
+    return this.authService.signup()
+  }
+
+  @Post('signin')
+  signin(){
+    return this.authService.signin()
+  }
+}
+
+
+// dto - data transfer object
+
+
+/* 
+
+one way of gettting req in controllers but it only work with express.
+
+  signup(@Req() req: Request){
+    console.log({reqbody: req.body})
+    return this.authService.signup()
+  } 
+
+*/
